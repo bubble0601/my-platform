f0d93407d6b382b875ef4591ec2ee63c183eaf8d
Awaka Ryohei <bubble0601@hotmail.co.jp>
2020-06-06 01:41:20 +0900

    fix script

--- scripts/convert_id3.rb
+++ scripts/convert_id3.rb
@@ -1,19 +1,39 @@
-require_relative '../app/config'
-require_relative '../models/utils/mp3'
+require './app/config'
+require './lib/music/mp3'
+
+puts "start convert mp3tag to ID3v2.4/UTF-8...\n"
 
 Dir["#{CONF.storage.music}/**/*.mp3"].each do |f|
-  tags = ID3.new(f)
-  tags.update_to_v24
-  tags.to_utf8
-  tags.save
+  begin
+    tags = ID3.new(f)
+    tags.update_to_v24
+    tags.to_utf8
+    tags.save
+  rescue => e
+    puts f
+    p e
+    puts
+  end
 end
 
+puts "confirm converted...\n"
+
 Dir["#{CONF.storage.music}/**/*.mp3"].each do |f|
-  tags = ID3.new(f)
   begin
-    raise if tags.getall('TIT2')[0].encoding != PyMP3::Encoding.UTF8
-    raise if tags.getall('TPE2')[0].encoding != PyMP3::Encoding.UTF8
-  rescue
+    tags = ID3.new(f)
+    begin
+      tit2 = tags.getall('TIT2')
+      tpe2 = tags.getall('TPE2')
+      raise if tit2.length > 0 and tit2[0].encoding != PyMP3::Encoding.UTF8
+      raise if tpe2.length > 0 and tpe2[0].encoding != PyMP3::Encoding.UTF8
+    rescue
+      puts f
+      p tags.get_all(true)
+      puts
+    end
+  rescue => e
     puts f
+    p e
+    puts
   end
 end
